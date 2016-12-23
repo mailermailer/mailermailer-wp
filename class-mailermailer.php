d1da29af8661aa55f36a6640ca98ef2171ba3783
Kevin H. Kamel <kamelkev@underprint.com>
2016-12-22 17:24:29 -0700

    Bump up version for compatibility with wp 4.7, while here update Copyright lines to reflect new year
    MM-15048

--- class-mailermailer.php
+++ class-mailermailer.php
@@ -25,7 +25,7 @@ class MailerMailer
 { 
 
   // Plugin version, used for cache-busting of style and script file references.
-  protected $version = '1.2.3';
+  protected $version = '1.2.4';
 
   // Unique identifier for your plugin.
   protected $plugin_slug = 'mailermailer';
